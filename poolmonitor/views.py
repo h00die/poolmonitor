f68ebd7f6f4c52539fda3951ea0fa0a32df488c3
Mike <mike@stcyrsecurity.com>
2015-05-12 17:53:20 -0400

    import os

--- poolmonitor/views.py
+++ poolmonitor/views.py
@@ -5,7 +5,7 @@ from django import forms
 import re
 from django.core.urlresolvers import reverse_lazy
 
-import tempfile
+import tempfile, os
 os.environ['MPLCONFIGDIR'] = tempfile.mkdtemp()
 import matplotlib.pyplot as plt
 
