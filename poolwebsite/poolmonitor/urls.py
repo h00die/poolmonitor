1ccba722e3ea9aacebbe879f28b44169abbc6971
Mike <mike@stcyrsecurity.com>
2015-05-24 20:49:22 -0400

    adding alpha characters to urls sensor view

--- poolwebsite/poolmonitor/urls.py
+++ poolwebsite/poolmonitor/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^sensor/(?P<sensor_file_system_location>[\-0-9]+)/$', views.detail, name='detail'),
+    url(r'^sensor/(?P<sensor_file_system_location>[\-0-9a-f]+)/$', views.detail, name='detail'),
     url(r'^sensor/add/$', views.SensorCreate.as_view(), name='create'),
     url(r'^sensor/update/(?P<pk>[\-0-9]+)/$', views.SensorUpdate.as_view(), name='edit'),
     url(r'^sensor/delete/(?P<pk>[\-0-9]+)/$', views.SensorDelete.as_view(), name='delete'),
